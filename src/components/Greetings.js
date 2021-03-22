4411028481b025fc28112432cd083ec1c1986c3c
Laurate S <may.sanejo@yahoo.com>
2021-03-22 12:48:45 +0000

    Greetings content has been added

---
+++ src/components/Greetings.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Greetings extends Component {
+    render() {
+        return (
+            <div className="site-blocks-cover" style="overflow: hidden;">
+                <div className="container">
+                    <div className="row align-items-center justify-content-center">
+                        <div className="col-md-12" style="position: relative;" data-aos="fade-up" data-aos-delay="200">
+                            <img src="images/code_main.svg" alt="Image" className="img-fluid img-absolute" width="800px"></img>
+                            <div className="row mb-4" data-aos="fade-up" data-aos-delay="200">
+                                <div className="col-lg-6 mr-auto">
+                                    <h1>Hi, I'm Laurate </h1>
+                                    <p className="mb-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Interested in building usable,
+                                useful, and well-constructed websites. I build amazing website from scratch. <span className="text-primary">Wanna hire me? </span> </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+export default Greetings;
