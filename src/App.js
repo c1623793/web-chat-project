426ac600757531187847b52e9ccba0e3a0311f16
Laurate S <may.sanejo@yahoo.com>
2021-03-22 12:51:03 +0000

    Removed redundant code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300"></body>
   );
 }
 
