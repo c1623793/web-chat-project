0562588a56d93b47d8bf14587716fba7f4f78199
Laurate S <may.sanejo@yahoo.com>
2021-03-22 14:41:51 +0000

    removed css propertieis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import React from 'react';
+import './css/App.css';
+import Body from './components/Body';
+import Sidebar from './components/Sidebar';
+import Navigation from './components/Navigation';
+import Greetings from './components/Greetings';
+import Projects from './components/Projects';
+import Form from './components/Form';
 
 function App() {
+
   return (
-    <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300"></body>
+    <div className="App">
+      <Body>
+        <Sidebar></Sidebar>
+        <Navigation></Navigation>
+        <Greetings>
+        </Greetings>
+        <Projects></Projects>
+        <Form></Form>
+      </Body>
+    </div>
+
   );
 }
 
